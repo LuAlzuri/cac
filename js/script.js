9484de38dc6efdeb76b0b275d4a06834a0ea2d31
Luciana Alzuri <alzuriluciana@gmail.com>
2023-10-23 20:04:43 -0300

    cambios en slider

--- js/script.js
+++ js/script.js
@@ -1,1 +1,22 @@
 //alert("Estoy funcionando")
+const testimonials = document.querySelectorAll('.testimonial');
+let indice = 0;
+
+function showTestimonial(index) {
+    testimonials.forEach((testimonial, i) => {
+        testimonial.style.display = i === index ? 'block' : 'none';
+    });
+}
+
+showTestimonial(indice);
+
+document.getElementById('prevBtn').addEventListener('click', () => {
+    indice = (indice - 1 + testimonials.length) % testimonials.length;
+    showTestimonial(indice);
+});
+
+document.getElementById('nextBtn').addEventListener('click', () => {
+    indice = (indice + 1) % testimonials.length;
+    showTestimonial(indice);
+});
+
